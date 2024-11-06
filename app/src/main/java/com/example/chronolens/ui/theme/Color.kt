e84f2ad4f471ea8ad2ae74164bfa26ea8f947a1e
lucasverdelho <PG50221@alunos.uminho.pt>
2024-11-06 21:34:44 +0000

    Added colors

--- app/src/main/java/com/example/chronolens/ui/theme/Color.kt
+++ app/src/main/java/com/example/chronolens/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.example.chronolens.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val dark_red = Color(0xFFA0A0FF)
+val dark_blue = Color(0xFD93FFFF)
+
+val light_green = Color(0x92FFA9FF)
+val light_red = Color(0xFF5555FF)
+val light_blue = Color(0x84EDFFFF)
+val light_yellow = Color(0xFFECADFF)
+val light_pink = Color(0xFD93FFFF)
