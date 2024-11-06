86813f5f1bdf639a0d7061efb27223cb6fa3a608
lucasverdelho <PG50221@alunos.uminho.pt>
2024-11-06 23:36:34 +0000

    Improved Login screen, background is temporarily black only

--- app/src/main/java/com/example/chronolens/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/chronolens/ui/screens/LoginScreen.kt
@@ -2,22 +2,32 @@ package com.example.chronolens.ui.screens
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
@@ -26,14 +36,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.chronolens.R
+import com.example.chronolens.ui.theme.defaultButtonColors
 import com.example.chronolens.viewModels.UserLoginState
 import com.example.chronolens.viewModels.UserState
 import com.example.chronolens.viewModels.UserViewModel
@@ -41,6 +55,7 @@ import com.example.chronolens.viewModels.UserViewModel
 
 
 
+import androidx.compose.ui.graphics.Brush
 
 @Composable
 fun LoginScreen(
@@ -48,8 +63,20 @@ fun LoginScreen(
     userState: State<UserState>,
     grantAccess: () -> Unit
 ) {
+    val colorScheme = MaterialTheme.colorScheme
 
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+    val gradientBrush = Brush.horizontalGradient(
+        colors = listOf(
+            colorScheme.primary,
+            colorScheme.secondary
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = gradientBrush)
+    ) {
         when (userState.value.userLoginState) {
             UserLoginState.Loading -> DisplayLoading()
             UserLoginState.LoggedIn -> grantAccess()
@@ -60,12 +87,14 @@ fun LoginScreen(
     }
 }
 
+
+
+
 @Composable
 fun LoginPrompt(
     viewModel: UserViewModel,
     userState: State<UserState>
 ) {
-
     val colorScheme = MaterialTheme.colorScheme
 
     var server by remember { mutableStateOf(viewModel.getServer()) }
@@ -74,57 +103,54 @@ fun LoginPrompt(
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.background(color = colorScheme.background)
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorScheme.background)
+            .padding(16.dp)
     ) {
-
         Image(
             painter = painterResource(id = R.drawable.large_logo),
             contentDescription = stringResource(R.string.app_name),
             modifier = Modifier.size(200.dp)
         )
 
-        Spacer(modifier = Modifier.padding(10.dp))
+        Spacer(modifier = Modifier.height(10.dp))
 
-        TextField(
+        CustomTextField(
             value = server,
             onValueChange = { server = it },
-            label = { Text("Server", color = colorScheme.onBackground) },
+            label = "Server",
             isError = userState.value.userLoginState == UserLoginState.CredentialsWrong,
-            singleLine = true,
-            enabled = userState.value.userLoginState != UserLoginState.Loading,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Uri,
-                imeAction = ImeAction.Next
-            )
+            keyboardType = KeyboardType.Uri,
+            imeAction = ImeAction.Next
         )
-        Spacer(modifier = Modifier.padding(10.dp))
-        TextField(
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        CustomTextField(
             value = username,
             onValueChange = { username = it },
-            label = { Text("Username", color = colorScheme.onBackground) },
+            label = "Username",
             isError = userState.value.userLoginState == UserLoginState.CredentialsWrong,
-            singleLine = true,
-            enabled = userState.value.userLoginState != UserLoginState.Loading,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next
-            )
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Next
         )
-        Spacer(modifier = Modifier.padding(10.dp))
-        TextField(
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        CustomTextField(
             value = password,
             onValueChange = { password = it },
-            label = { Text("Password", color = colorScheme.onBackground) },
-            visualTransformation = PasswordVisualTransformation(),
-            singleLine = true,
-            enabled = userState.value.userLoginState != UserLoginState.Loading,
+            label = "Password",
+            isPassword = true,
             isError = userState.value.userLoginState == UserLoginState.CredentialsWrong,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done
-            )
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Done
         )
-        Spacer(modifier = Modifier.padding(10.dp))
+
+        Spacer(modifier = Modifier.height(20.dp))
+
         Button(
             onClick = {
                 viewModel.login(server, username, password)
@@ -132,17 +158,92 @@ fun LoginPrompt(
                 password = ""
             },
             enabled = userState.value.userLoginState != UserLoginState.Loading,
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+            shape = RoundedCornerShape(4.dp),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            colors = defaultButtonColors()
         ) {
             if (userState.value.userLoginState == UserLoginState.Loading) {
                 Icon(imageVector = Icons.Filled.Refresh, contentDescription = null)
             } else {
-                Text("Login", color = colorScheme.onPrimary)
+                Text("Log In", color = colorScheme.secondaryContainer, style = MaterialTheme.typography.bodyMedium)
             }
         }
     }
 }
+
+
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    isPassword: Boolean = false,
+    isError: Boolean = false,
+    keyboardType: KeyboardType,
+    imeAction: ImeAction
+) {
+    val colorScheme = MaterialTheme.colorScheme
+    val labelColor = colorScheme.onBackground.copy(alpha = 0.7f)
+
+    val typography = MaterialTheme.typography
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+    ) {
+
+        Text(
+            text = label,
+            color = labelColor,
+            style = typography.labelSmall,
+            modifier = Modifier.padding(start = 4.dp, bottom = 4.dp)
+        )
+
+
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            singleLine = true,
+            visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction
+            ),
+            textStyle = typography.bodyLarge.copy(color = colorScheme.onBackground),
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp)
+                ) {
+
+                    if (value.isEmpty()) {
+                        Text(
+                            text = label,
+                            color = labelColor,
+                            style = typography.bodyMedium
+                        )
+                    }
+                    innerTextField()
+                }
+            }
+        )
+
+
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 0.dp),
+            thickness = 2.dp,
+            color = if (isError) Color.Red else colorScheme.onBackground.copy(alpha = 0.7f)
+        )
+    }
+}
+
+
+
 
 
 @Composable
@@ -151,8 +252,13 @@ fun DisplayLoading() {
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.background(color = colorScheme.background)
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorScheme.background)
+            .padding(16.dp)
     ) {
+
         Image(
             painter = painterResource(id = R.drawable.large_logo),
             contentDescription = stringResource(R.string.app_name),
