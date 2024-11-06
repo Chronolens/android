363aa41d56c685e0194143c1e29a85e3f25faed0
lucasverdelho <PG50221@alunos.uminho.pt>
2024-11-06 21:38:00 +0000

    working on login page ui

--- app/src/main/java/com/example/chronolens/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/chronolens/ui/screens/LoginScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -25,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -36,6 +38,10 @@ import com.example.chronolens.viewModels.UserLoginState
 import com.example.chronolens.viewModels.UserState
 import com.example.chronolens.viewModels.UserViewModel
 
+
+
+
+
 @Composable
 fun LoginScreen(
     viewModel: UserViewModel,
@@ -45,7 +51,7 @@ fun LoginScreen(
 
     Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
         when (userState.value.userLoginState) {
-            UserLoginState.Loading -> DisplayLoading(stringResource(R.string.app_name))
+            UserLoginState.Loading -> DisplayLoading()
             UserLoginState.LoggedIn -> grantAccess()
             else -> {
                 LoginPrompt(viewModel, userState)
@@ -60,21 +66,29 @@ fun LoginPrompt(
     userState: State<UserState>
 ) {
 
+    val colorScheme = MaterialTheme.colorScheme
+
     var server by remember { mutableStateOf(viewModel.getServer()) }
     var username by remember { mutableStateOf(viewModel.getUsername()) }
     var password by remember { mutableStateOf("") }
 
-    Column {
-        Text(
-            text = stringResource(R.string.app_name),
-            fontSize = 40.sp,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background(color = colorScheme.background)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.large_logo),
+            contentDescription = stringResource(R.string.app_name),
+            modifier = Modifier.size(200.dp)
         )
+
         Spacer(modifier = Modifier.padding(10.dp))
+
         TextField(
             value = server,
             onValueChange = { server = it },
-            label = { Text("Server") },
+            label = { Text("Server", color = colorScheme.onBackground) },
             isError = userState.value.userLoginState == UserLoginState.CredentialsWrong,
             singleLine = true,
             enabled = userState.value.userLoginState != UserLoginState.Loading,
@@ -87,7 +101,7 @@ fun LoginPrompt(
         TextField(
             value = username,
             onValueChange = { username = it },
-            label = { Text("Username") },
+            label = { Text("Username", color = colorScheme.onBackground) },
             isError = userState.value.userLoginState == UserLoginState.CredentialsWrong,
             singleLine = true,
             enabled = userState.value.userLoginState != UserLoginState.Loading,
@@ -100,7 +114,7 @@ fun LoginPrompt(
         TextField(
             value = password,
             onValueChange = { password = it },
-            label = { Text("Password") },
+            label = { Text("Password", color = colorScheme.onBackground) },
             visualTransformation = PasswordVisualTransformation(),
             singleLine = true,
             enabled = userState.value.userLoginState != UserLoginState.Loading,
@@ -124,30 +138,30 @@ fun LoginPrompt(
             if (userState.value.userLoginState == UserLoginState.Loading) {
                 Icon(imageVector = Icons.Filled.Refresh, contentDescription = null)
             } else {
-                Text("Login")
+                Text("Login", color = colorScheme.onPrimary)
             }
-//        if (userState.value.userLoginState == UserLoginState.Error) {
-//            AlertDialogTemplate(
-//                onDismiss = onDismiss,
-//                dialogTitle = stringResource(R.string.loginErrorTitle),
-//                dialogText = stringResource(R.string.loginErrorMessage)
-//            )
-//        }
         }
     }
 }
 
 
 @Composable
-fun DisplayLoading(appName: String) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = appName,
-            fontSize = 40.sp,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+fun DisplayLoading() {
+    val colorScheme = MaterialTheme.colorScheme
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background(color = colorScheme.background)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.large_logo),
+            contentDescription = stringResource(R.string.app_name),
+            modifier = Modifier.size(200.dp)
         )
+
         CircularProgressIndicator(
             modifier = Modifier.width(64.dp),
+            color = colorScheme.primary
         )
     }
 }
