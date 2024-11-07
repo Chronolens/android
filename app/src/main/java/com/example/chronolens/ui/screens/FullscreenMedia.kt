9a9cb07581b717503a1dc2e0463f4d00a2f030aa
lucasverdelho <PG50221@alunos.uminho.pt>
2024-11-07 19:40:43 +0000

    Image View is working as intended, full photo quality and zoom, TODO: Fill metadata box

--- app/src/main/java/com/example/chronolens/ui/screens/FullscreenMedia.kt
+++ app/src/main/java/com/example/chronolens/ui/screens/FullscreenMedia.kt
@@ -2,13 +2,11 @@ package com.example.chronolens.ui.screens
 
 import android.util.Log
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.TransformableState
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -25,16 +23,16 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import coil3.compose.AsyncImage
+import coil3.compose.rememberAsyncImagePainter
 import com.example.chronolens.models.LocalMedia
 import com.example.chronolens.models.MediaAsset
 import com.example.chronolens.models.RemoteMedia
@@ -69,6 +67,7 @@ fun FullscreenMediaView(
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.Black)
     ) {
 
         // The full image component
@@ -145,66 +144,50 @@ fun LoadFullImage(
     hideBox: () -> Unit,
     showBox: () -> Unit
 ) {
-    val scale = remember { mutableStateOf(1f) }
-    val offset = remember { mutableStateOf(Offset(0f, 0f)) }
 
-    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
-    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-    val density = LocalDensity.current
-
-    val transformableState = rememberTransformableState { zoomChange, panChange, _ ->
-
-        scale.value = (scale.value * zoomChange).coerceIn(1f, 4f)
-
-        val maxXOffset = with(density) { (screenWidth * (scale.value - 1)).toPx() / 2 }
-        val maxYOffset = with(density) { (screenHeight * (scale.value - 1)).toPx() / 2 }
-
-        offset.value = Offset(
-            x = (offset.value.x + panChange.x * scale.value).coerceIn(-maxXOffset, maxXOffset),
-            y = (offset.value.y + panChange.y * scale.value).coerceIn(-maxYOffset, maxYOffset)
-        )
-
-    }
-
-    val maxZoom = 2f
-    val defaultZoom = 1f
-
-    // Double-tap gesture for zoom
-    val doubleTapModifier = Modifier.pointerInput(Unit) {
-        detectTapGestures(
-            onDoubleTap = { tapOffset ->
-                val imageWidth = screenWidth.toPx() * scale.value
-                val imageHeight = screenHeight.toPx() * scale.value
-
-                if (scale.value > defaultZoom) {
-                    scale.value = defaultZoom
-                    offset.value = Offset.Zero
-                } else {
-                    scale.value = maxZoom
-
-                    val maxXOffset = with(density) { (screenWidth * (scale.value - 1)).toPx() / 2 }
-                    val maxYOffset = with(density) { (screenHeight * (scale.value - 1)).toPx() / 2 }
-
-                    offset.value = Offset(
-                        x = (screenWidth.toPx() / 2 - (tapOffset.x * scale.value - imageWidth / 2)).coerceIn(
-                            -maxXOffset,
-                            maxXOffset
-                        ),
-                        y = (screenHeight.toPx() / 2 - (tapOffset.y * scale.value - imageHeight / 2)).coerceIn(
-                            -maxYOffset,
-                            maxYOffset
-                        )
-                    )
-                }
+    if (mediaAsset is RemoteMedia) {
+        var imageUrl by remember { mutableStateOf<String?>(null) }
+        LaunchedEffect(mediaAsset) {
+            val url = viewModel.getRemoteAssetFullImageUrl(mediaAsset.id)
+            imageUrl = url
+        }
+        if (imageUrl != null) {
+            ImageDisplay(
+                photoURI = imageUrl!!,
+                hideBox = hideBox,
+                showBox = showBox
+            )
+        } else {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
             }
+        }
+    } else if (mediaAsset is LocalMedia) {
+        ImageDisplay(
+            photoURI = mediaAsset.path,
+            hideBox = hideBox,
+            showBox = showBox
         )
     }
+}
 
-    // Separate vertical drag modifier to handle swipe gestures when fully zoomed out
+
+
+
+@Composable
+private fun ImageDisplay (
+    photoURI : String,
+    modifier: Modifier = Modifier,
+    hideBox: () -> Unit,
+    showBox: () -> Unit){
+
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    var zoom by remember { mutableStateOf(1f) }
+
     val verticalDragModifier = Modifier.pointerInput(Unit) {
         detectVerticalDragGestures(
             onVerticalDrag = { _, dragAmount ->
-                if (scale.value == defaultZoom) {  // Only allow swipe gestures when fully zoomed out
+                if (zoom == 1f) {  // Only allow swipe gestures when fully zoomed out
                     if (dragAmount < -10) {
                         showBox()
                     } else if (dragAmount > 10) {
@@ -216,69 +199,79 @@ fun LoadFullImage(
     }
 
 
-    if (mediaAsset is RemoteMedia) {
-        var imageUrl by remember { mutableStateOf<String?>(null) }
-        LaunchedEffect(mediaAsset) {
-            val url = viewModel.getRemoteAssetFullImageUrl(mediaAsset.id)
-            imageUrl = url
-        }
-        if (imageUrl != null) {
-            ZoomableImage(
-                url = imageUrl!!,
-                scale = scale.value,
-                offset = offset.value,
-                transformableState = transformableState,
-                modifier = verticalDragModifier.then(doubleTapModifier)
-            )
-        } else {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                CircularProgressIndicator()
-            }
-        }
-    } else if (mediaAsset is LocalMedia) {
-        ZoomableImage(
-            url = mediaAsset.path,
-            scale = scale.value,
-            offset = offset.value,
-            transformableState = transformableState,
-
-            modifier = doubleTapModifier.then(verticalDragModifier)
-        )
-    }
-}
-
-@Composable
-fun ZoomableImage(
-    url: String,
-    scale: Float,
-    offset: Offset,
-    transformableState: TransformableState,
-    modifier: Modifier
-) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black)
-            .graphicsLayer(
-                scaleX = scale,
-                scaleY = scale,
-                translationX = offset.x,
-                translationY = offset.y
-            )
-            .transformable(state = transformableState)
-            .then(modifier)
+            .then(verticalDragModifier)
+    ){
+        Image(
+            painter = rememberAsyncImagePainter(photoURI),
+            contentDescription = "",
+            modifier = modifier
+                .pointerInput(Unit){
+                    detectTapGestures(
+                        onDoubleTap = { tapOffset ->
+                            zoom = if (zoom > 1f) 1f else 2f
+                            offset = calculateDoubleTapOffset(zoom, size, tapOffset)
+                        }
+                    )
 
-    ) {
-        AsyncImage(
-            model = url,
-            contentDescription = "Zoomable Image",
-            contentScale = ContentScale.Fit,
-            modifier = Modifier
+                }
+                .pointerInput(Unit) {
+                    detectTransformGestures { centroid, pan, gestureZoom, _ ->
+                        offset = offset.calculateNewOffset(
+                            centroid, pan, zoom, gestureZoom, size
+                        )
+                        zoom = maxOf(1f, zoom * gestureZoom)
+
+                    }
+                }
+                .graphicsLayer {
+                    translationX = -offset.x * zoom
+                    translationY = -offset.y * zoom
+                    scaleX = zoom; scaleY = zoom
+                    transformOrigin = TransformOrigin(0f, 0f)
+                }
+                .then(verticalDragModifier)
                 .align(Alignment.Center)
-
+                .clipToBounds()
         )
     }
+
 }
+
+
+
+
+fun Offset.calculateNewOffset(
+    centroid: Offset,
+    pan: Offset,
+    zoom: Float,
+    gestureZoom: Float,
+    size: IntSize
+): Offset {
+    val newScale = maxOf(1f, zoom * gestureZoom)
+    val newOffset = (this + centroid / zoom) -
+            (centroid / newScale + pan / zoom)
+    return Offset(
+        newOffset.x.coerceIn(0f, (size.width / zoom) * (zoom - 1f)),
+        newOffset.y.coerceIn(0f, (size.height / zoom) * (zoom - 1f))
+    )
+}
+
+fun calculateDoubleTapOffset(
+    zoom: Float,
+    size: IntSize,
+    tapOffset: Offset
+): Offset {
+    val newOffset = Offset(tapOffset.x, tapOffset.y)
+    return Offset(
+        newOffset.x.coerceIn(0f, (size.width / zoom) * (zoom - 1f)),
+        newOffset.y.coerceIn(0f, (size.height / zoom) * (zoom - 1f))
+    )
+}
+
+
 
 
 @Composable
