53e443f544bd482abe416bbd73f4eb049c64227c
lucasverdelho <PG50221@alunos.uminho.pt>
2024-11-15 14:27:33 +0000

    Removed exifthumbnail as it was never used

--- app/src/main/java/com/example/chronolens/viewModels/MediaGridScreenViewModel.kt
+++ app/src/main/java/com/example/chronolens/viewModels/MediaGridScreenViewModel.kt
@@ -4,23 +4,18 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import coil3.BitmapImage
-import com.example.chronolens.utils.SyncManager
 import com.example.chronolens.models.LocalMedia
 import com.example.chronolens.models.MediaAsset
+import com.example.chronolens.models.Person
 import com.example.chronolens.models.RemoteMedia
 import com.example.chronolens.repositories.MediaGridRepository
+import com.example.chronolens.utils.SyncManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import android.graphics.Bitmap
-import android.media.ExifInterface
-import com.example.chronolens.models.Person
-import com.example.chronolens.utils.APIUtils
-import java.io.File
-import java.io.IOException
 
 data class MediaGridState(
     val media: List<MediaAsset> = listOf(),
@@ -97,7 +92,6 @@ class MediaGridScreenViewModel(private val mediaGridRepository: MediaGridReposit
             for (media in localMediaNotCalculated) {
                 val checksum = mediaGridRepository.getOrComputeChecksum(media.id, media.path)
                 media.checksum = checksum
-                media.thumbnail = loadExifThumbnail(media)
                 localMediaCalculated += media
             }
             localAssets += localMediaCalculated
@@ -180,17 +174,6 @@ class MediaGridScreenViewModel(private val mediaGridRepository: MediaGridReposit
         }
     }
 
-
-    private fun loadExifThumbnail(localMedia: LocalMedia): Bitmap? {
-        return try {
-            val file = File(localMedia.path)
-            val exifInterface = ExifInterface(file)
-            exifInterface.thumbnailBitmap
-        } catch (e: IOException) {
-            Log.e("MediaGridScreenViewModel", "Failed to load EXIF thumbnail: ${e.message}")
-            null
-        }
-    }
 
     fun loadPeople() {
         viewModelScope.launch {
