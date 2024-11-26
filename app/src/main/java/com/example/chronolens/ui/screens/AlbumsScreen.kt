14b06e652a9082e7e30a28c3023abe3d7c8e1b3b
lucasverdelho <PG50221@alunos.uminho.pt>
2024-11-26 13:15:59 +0000

    Redesigned Dialog Box for the creation of faces

--- app/src/main/java/com/example/chronolens/ui/screens/AlbumsScreen.kt
+++ app/src/main/java/com/example/chronolens/ui/screens/AlbumsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.chronolens.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
@@ -16,11 +17,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -35,14 +43,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import com.example.chronolens.models.KnownPerson
 import com.example.chronolens.models.Person
@@ -113,9 +124,10 @@ fun AlbumsScreen(
         if (showNameDialog) {
             NameInputDialog(
                 onDismiss = { viewModel.dismissNameDialog() },
-                onConfirm = { personName ->
-                    viewModel.onNameConfirmed(personName)
-                }
+                onConfirm = { personName -> viewModel.onNameConfirmed(personName) },
+                selectedPeopleCount = selectedPeople.size, 
+                primaryColor = MaterialTheme.colorScheme.primary,
+                secondaryColor = MaterialTheme.colorScheme.secondary
             )
         }
     }
@@ -201,45 +213,116 @@ fun PersonItem(
     }
 }
 
-
-// TODO: Fix for HEIF photos, as they are not being displayed probably due to the conversion to bitmap
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NameInputDialog(
     onDismiss: () -> Unit,
-    onConfirm: (String) -> Unit
+    onConfirm: (String) -> Unit,
+    selectedPeopleCount: Int,
+    primaryColor: Color = MaterialTheme.colorScheme.primary,
+    secondaryColor: Color = MaterialTheme.colorScheme.secondary
 ) {
     var personName by remember { mutableStateOf("") }
 
-    AlertDialog(
+    BasicAlertDialog(
         onDismissRequest = onDismiss,
-        title = { Text("Name Unknown Persons") },
-        text = {
-            TextField(
-                value = personName,
-                onValueChange = { personName = it },
-                label = { Text("Person Name") }
-            )
-        },
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    if (personName.isNotBlank()) {
-                        onConfirm(personName)
-                        onDismiss()
+        modifier = Modifier
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(primaryColor, secondaryColor)
+                ),
+                shape = RoundedCornerShape(8.dp)
+            ),
+        properties = DialogProperties(),
+        content = {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+
+                Text(
+                    text = if (selectedPeopleCount == 1) "Rename" else "Group People",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Black.copy(alpha = 0.4f), shape = RoundedCornerShape(4.dp))
+                        .padding(12.dp)
+                ) {
+                    BasicTextField(
+                        value = personName,
+                        onValueChange = { personName = it },
+                        textStyle = LocalTextStyle.current.copy(color = Color.White),
+                        cursorBrush = SolidColor(Color.White),
+                        decorationBox = { innerTextField ->
+                            if (personName.isEmpty()) {
+                                Text(
+                                    text = "Person Name",
+                                    style = MaterialTheme.typography.bodySmall.copy(color = Color.White.copy(alpha = 0.5f)),
+                                    modifier = Modifier.padding(start = 4.dp, top = 4.dp)
+                                )
+                            }
+                            innerTextField()
+                        }
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        onClick = onDismiss,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                        ),
+                        shape = RoundedCornerShape(4.dp),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text("Cancel")
+                    }
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    Button(
+                        onClick = {
+                            if (personName.isNotBlank()) {
+                                onConfirm(personName)
+                                onDismiss()
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                        ),
+                        shape = RoundedCornerShape(4.dp),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text("Confirm")
                     }
                 }
-            ) {
-                Text("Confirm")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text("Cancel")
             }
         }
     )
 }
+
+
+
+
+
+
+// TODO: Fix for HEIF photos, as they are not being displayed probably due to the conversion to bitmap
 
 
 @Composable
